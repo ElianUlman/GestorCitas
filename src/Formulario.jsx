190b2bf5a977033b73fb0650960a32b28db3f936
Android Studio <devandroid@ort>
2026-04-07 15:08:30 -0300

    guardado de datos del formulario

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,29 +1,54 @@
+import { useState } from 'react'
 import './Formulario.css'
+
 function Formulario() {
+
+    const [cita, setCita] = useState({
+        mascota: "",
+        propietario: "",
+        fecha: "",
+        hora: "",
+        sintomas: ""
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        //if
+        setCita("")
+
+        console.log(cita)
+    }
+
+    const handleChange = (e) => {
+        setCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
+        <div className="one-half column">
+            <h2>Crear mi Cita</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Nombre Mascota</label>
+                <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" onChange={handleChange} />
 
-                    <div className="one-half column">
-                        <h2>Crear mi Cita</h2>
-                        <form>
-                            <label>Nombre Mascota</label>
-                            <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" />
+                <label>Nombre Dueño</label>
+                <input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" onChange={handleChange} />
 
-                            <label>Nombre Dueño</label>
-                            <input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" />
+                <label>Fecha</label>
+                <input type="date" name="fecha" className="u-full-width" onChange={handleChange} />
 
-                            <label>Fecha</label>
-                            <input type="date" name="fecha" className="u-full-width" />
+                <label>hora</label>
+                <input type="time" name="hora" className="u-full-width" onChange={handleChange} />
 
-                            <label>hora</label>
-                            <input type="time" name="hora" className="u-full-width" />
+                <label>Sintomas</label>
+                <textarea name="sintomas" className="u-full-width" onChange={handleChange}></textarea>
 
-                            <label>Sintomas</label>
-                            <textarea name="sintomas" className="u-full-width"></textarea>
+                <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
+            </form>
+        </div>
 
-                            <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
-                        </form>
-                    </div>
-               
     )
 }
 
